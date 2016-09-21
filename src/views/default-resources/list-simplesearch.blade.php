7e5b9ea9c7006b8a0256f51fb332a1fd3287e5ad
Niels Filmer <niels.filmer@boredofadvisors.com>
2016-09-21 10:56:16 +0200

    Added total and simplesearch template

--- src/views/default-resources/list-simplesearch.blade.php
+++ src/views/default-resources/list-simplesearch.blade.php
@@ -14,6 +14,14 @@
         <div class="panel-heading">
             {{ $heading }} ({{ $total }})
             @if(isset($show_add) && $show_add)<a class="btn btn-primary btn-xs pull-right" href="{{ $list->getCreateUrl() }}">New {{ $object_name }}</a>@endif
+            <form method="GET" class="form-heading-quicksearch col-xs-6 pull-right" style="position: relative; top: -7px;">
+                <div class="input-group input-group-sm">
+                    <input type="text" name="filter" class="form-control" placeholder="Zoeken" value="{{ $filter }}">
+                    <span class="input-group-btn">
+                        <button class="btn btn-default" type="submit">Go!</button>
+                    </span>
+                </div>
+            </form>
         </div>
     @endif
 
