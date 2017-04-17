03049ef731009c7a7de6a5922bff3642194a333d
Niels Filmer <niels@angrymen.nl>
2017-04-17 16:20:40 +0200

    Added optional disk to handle uploads

--- src/NielsFilmer/CmsPackage/Jobs/HandleUploadedImage.php
+++ src/NielsFilmer/CmsPackage/Jobs/HandleUploadedImage.php
@@ -33,19 +33,26 @@ class HandleUploadedImage
     protected $quality;
 
     /**
+     * @var string
+     */
+    protected $disk;
+
+    /**
      * Create a new job instance.
      *
      * @param UploadedFile $file
      * @param array $config
      * @param $extension
      * @param int $quality
+     * @param string $disk
      */
-    public function __construct(UploadedFile $file, array $config, $extension = null, $quality = 90)
+    public function __construct(UploadedFile $file, array $config, $extension = null, $quality = 90, $disk = 'cloud')
     {
         $this->config = $config;
         $this->file = $file;
         $this->extension = $extension;
         $this->quality = $quality;
+        $this->disk = $disk;
     }
 
     /**
@@ -68,9 +75,15 @@ class HandleUploadedImage
 
         $filename   = bin2hex( openssl_random_pseudo_bytes( 16 ) ) . ".{$extension}";
         $image_path = "{$this->config['path']}/{$filename}";
-        $storage->cloud()->put( $image_path, fopen( $local_path, 'r' ) );
-        // todo: WHY O WHY DOES THIS DO Text File Busy on my windows machine
-//            unlink( $local_path );
+
+        $disk = ($this->disk == 'cloud') ? $storage->cloud() : $storage->disk($this->disk);
+        $disk->put( $image_path, fopen( $local_path, 'r' ) );
+
+        try {
+            unlink( $local_path );
+        } catch(\Exception $e) {
+            // Failed to delete local file. Manual cleaning required.
+        }
 
         return $image_path;
     }
